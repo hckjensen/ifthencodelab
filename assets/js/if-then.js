93fc46a549a6fd2a135782edf116a908942485da
hckjensen <hckjensen96@gmail.com>
2023-09-04 12:49:38 +0200

    ifthen/operators

--- assets/js/if-then.js
+++ assets/js/if-then.js
@@ -8,33 +8,54 @@ const E = 40;
 
 // Opgave 1: skriv et expression der viser true når A er lig med B.
 
-if ( /*..dit expression Her*/ ) {
+if (A==B) {
     console.log('statement true!');
+}else{
+    console.log('statement false')
 }
 
 
 //opgave 2: skrive en If then statement der vurderer om A er større end B.
 
-
+if (A>B) {
+    console.log('A is bigger than B')
+}else if(B>A){
+    console.log('B is bigger than A')
+}
 
 
 //opgave 3: Skriv en If then statement der vurderer om A er mindre end eller lig med B
 
 
-
+if (A<=B) {
+    console.log('statement true')
+} else {
+    console.log('statement false');
+}
 
 
 //opgave 4: Skriv en If then statement der bliver FALSE når du sammenligner B og C.
 
 
-
+if (B===C){
+    console.log('true')
+}else{
+    console.log('false');
+}
 
 
 //opgave 5: Skriv en If then statement der bliver TRUE når du sammenligner B og C.
 
-
+if (B==C){
+    console.log('true')
+}else{
+    console.log('false');
+}
 
 
 //opgave 6: Skriv en If then statement der sammenligner B = C og E = A hvis B=C skal den blive TRUE. 
 
+if (B==C && E!=A){
+    console.log('TRUE')
 
+}
--- assets/js/operators.js
+++ assets/js/operators.js
@@ -8,20 +8,28 @@ const E = 40;
 
 // Opgave 1: skriv et expression der viser true når A er lig med B eller A er lig med D.
 
-if ( /*..dit expression Her*/ ) {
+if (A==B || A==D) {
     console.log('statement true!');
 }
 
 
 //opgave 2: skrive en If then statement der bliver TRUE når  A er større end B og A er mindre end E.
 
-
+if (A>B && A<E){
+    console.log('statement true');
+}
 
 
 //opgave 3: Skriv en If then statement med  && og  || der kun er sand når alle expressions er sande.
 
-
+if (A!=B && B==C || E==40){
+    console.log('statement true');
+}
 
 
 
 //opgave 4: Skriv en If then statement der kan finde ud af om F er "undefined" eller har en værdi, ved at bruge !.
+
+if(F!=''){
+    console.log('statement true');
+}
